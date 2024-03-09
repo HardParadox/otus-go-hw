07fd5982c810c172921a1555161a96d34694875d
HardParadox <toblerone0073@gmail.com>
2024-03-10 01:22:34 +0300

    HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for key, value := range env {
+		if value.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		err := os.Setenv(key, value.Value)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	c := exec.Command(cmd[0], cmd[1:4]...)
+
+	c.Stdout = os.Stdout
+
+	if err := c.Run(); err != nil {
+		panic(err)
+	}
+
+	return c.ProcessState.ExitCode()
 }
